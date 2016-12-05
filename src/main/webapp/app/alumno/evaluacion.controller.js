19f0462998bee3085500f4e724da4e7d920ab7e1
rodrigo.denis <rodrigo.denis@SG_SICEI_142144.inet.uady.mx>
2016-12-05 09:14:18 -0600

    #3895 feedback a los cuestionarios: se avisa al usuario cuales preguntas no han sido contestadas

--- src/main/webapp/app/alumno/evaluacion.controller.js
+++ src/main/webapp/app/alumno/evaluacion.controller.js
@@ -12,6 +12,13 @@
     	vm.show = 0;
     	vm.save=save;
     	vm.resuelto=null;
+    	vm.anterior=anterior;
+    	vm.siguiente=siguiente;
+    	vm.validarseccion=validarseccion;
+    	vm.valido=false;
+    	vm.mensaje=null;
+    	vm.preguntainvalida=[];
+    	vm.contain=contain;
         CuestionarioAlumnoService.obtener({
     		ambito : $state.params.ambito
         }, onSuccess, onError);        
@@ -66,6 +73,69 @@
         function onSaveError(error) {
             AlertService.error(error.data.message);
             $state.go('alumno',{}, {reload: true});
+        }
+        
+        function anterior(indice){
+        	vm.validarseccion(indice);
+        	if(vm.valido)
+        	vm.show=indice;
+        	
+        }
+        function siguiente(indice){
+        	vm.validarseccion(indice);
+        	if(vm.valido)
+        	vm.show=indice+2;
+        	
+        }
+        
+        function validarseccion(seccion){
+        	var grupo=vm.resuelto.grupoPreguntasForUI;
+        	var cuestionario=vm.resuelto.cuestionarioResuelto;
+        	//var newRespuestas =[];
+        	var valido=true;
+        	var mensaje=null;
+        	var k=0;
+        	vm.preguntainvalida=[];
+        	//for(var i=0;i<grupo.length;i++){
+        	//alert(seccion);
+        	var index=0;
+        		for(var j=0;j<grupo[seccion].preguntasWrapperForUI.length;j++){
+        			for(var h=0;h<cuestionario.respuestasPregunta.length;h++){
+        				if(grupo[seccion].preguntasWrapperForUI[j].pregunta.obligatoria && cuestionario.respuestasPregunta[h].pregunta.id==grupo[seccion].preguntasWrapperForUI[j].pregunta.id){
+
+        					if(grupo[seccion].preguntasWrapperForUI[j].respuesta.respuestaSeleccionada==null){
+        						valido=false;
+        						vm.preguntainvalida[index]=grupo[seccion].preguntasWrapperForUI[j].pregunta.id;
+        						
+        						//break;
+        						index++;
+        					}
+        					
+        				}
+        				
+        			}
+        				
+        		}
+        		if(!valido){
+        			mensaje="No ha contestado todas las preguntas de esta sección";
+					//break;
+				}
+        	//}
+        		vm.valido=valido;
+        		vm.mensaje=mensaje;
+        }
+        
+        function contain(idpregunta){
+        	var invalido=false;
+        	//alert(vm.preguntainvalida.length);
+        	for(var index=0;index<vm.preguntainvalida.length;index++){
+        		if(vm.preguntainvalida[index]==idpregunta){
+        			invalido=true;
+        			break;
+        		}
+        			
+        	}
+        	return invalido;
         }
     }
 })();
--- src/main/webapp/app/docente/autoevaluacion.controller.js
+++ src/main/webapp/app/docente/autoevaluacion.controller.js
@@ -12,6 +12,13 @@
     	vm.show = 0;
     	vm.save = save;
     	vm.resuelto = null;
+    	vm.anterior=anterior;
+    	vm.siguiente=siguiente;
+    	vm.validarseccion=validarseccion;
+    	vm.valido=false;
+    	vm.mensaje=null;
+    	vm.preguntainvalida=[];
+    	vm.contain=contain;
         CuestionarioService.obtener({
     		ambito : $state.params.ambito, 
     		profesor : $state.params.profesor
@@ -64,6 +71,62 @@
         function onSaveError(error) {
             AlertService.error(error.data.message);
             $state.go('docente',{}, {reload: true});
+        }
+        
+        function anterior(indice){
+        	vm.validarseccion(indice);
+        	if(vm.valido)
+        	vm.show=indice;
+        	
+        }
+        function siguiente(indice){
+        	vm.validarseccion(indice);
+        	if(vm.valido)
+        	vm.show=indice+2;
+        	
+        }
+        
+        function validarseccion(seccion){
+        	var grupo=vm.resuelto.grupoPreguntasForUI;
+        	var cuestionario=vm.resuelto.cuestionarioResuelto;
+        	var valido=true;
+        	var mensaje=null;
+        	var k=0;
+        	vm.preguntainvalida=[];
+        	var index=0;
+        		for(var j=0;j<grupo[seccion].preguntasWrapperForUI.length;j++){
+        			for(var h=0;h<cuestionario.respuestasPregunta.length;h++){
+        				if(grupo[seccion].preguntasWrapperForUI[j].pregunta.obligatoria && cuestionario.respuestasPregunta[h].pregunta.id==grupo[seccion].preguntasWrapperForUI[j].pregunta.id){
+
+        					if(grupo[seccion].preguntasWrapperForUI[j].respuesta.respuestaSeleccionada==null){
+        						valido=false;
+        						vm.preguntainvalida[index]=grupo[seccion].preguntasWrapperForUI[j].pregunta.id;
+        						index++;
+        					}
+        					
+        				}
+        				
+        			}
+        				
+        		}
+        		if(!valido){
+        			mensaje="No ha contestado todas las preguntas de esta sección";
+				}
+        		vm.valido=valido;
+        		vm.mensaje=mensaje;
+        }
+        
+        function contain(idpregunta){
+        	var invalido=false;
+        	//alert(vm.preguntainvalida.length);
+        	for(var index=0;index<vm.preguntainvalida.length;index++){
+        		if(vm.preguntainvalida[index]==idpregunta){
+        			invalido=true;
+        			break;
+        		}
+        			
+        	}
+        	return invalido;
         }
     }
 })();
