434e0d002b310bc65a201659bae4a9e190151c88
Edith Quijano <rhueiden@gmail.com>
2016-12-12 13:26:19 -0600

    Cambios en el señalamiento de las preguntas obligatorias le faltan por contestar y otras modificaciones menores en diseño de las evaluaciones.

--- src/main/webapp/app/docente/autoevaluacion.controller.js
+++ src/main/webapp/app/docente/autoevaluacion.controller.js
@@ -12,20 +12,20 @@
     	vm.show = 0;
     	vm.save = save;
     	vm.resuelto = null;
-    	vm.anterior=anterior;
-    	vm.siguiente=siguiente;
-    	vm.validarseccion=validarseccion;
-    	vm.valido=false;
-    	vm.mensaje=null;
-    	vm.preguntainvalida=[];
-    	vm.contain=contain;
+    	vm.anterior = anterior;
+    	vm.siguiente = siguiente;
+    	vm.validarseccion = validarseccion;
+    	vm.valido = false;
+    	vm.mensaje = null;
+    	vm.preguntainvalida = [];
+    	vm.contain = contain;
+    	
         CuestionarioService.obtener({
     		ambito : $state.params.ambito, 
     		profesor : $state.params.profesor
         }, onSuccess, onError);        
 
-        function onSuccess(data, headers) {
-        	
+        function onSuccess(data, headers) {        	
             vm.resuelto = data;
         }
         
@@ -34,26 +34,24 @@
         }
         
         function save() {
-        	var grupo=vm.resuelto.grupoPreguntasForUI;
-        	var cuestionario=vm.resuelto.cuestionarioResuelto;
+        	var grupo = vm.resuelto.grupoPreguntasForUI;
+        	var cuestionario = vm.resuelto.cuestionarioResuelto;
 
         	var newRespuestas =[];
-        	var k=0;
-        	for(var i=0;i<grupo.length;i++){
-        		for(var j=0;j<grupo[i].preguntasWrapperForUI.length;j++){
-        			for(var h=0;h<cuestionario.respuestasPregunta.length;h++){
-        				if(cuestionario.respuestasPregunta[h].pregunta.id==grupo[i].preguntasWrapperForUI[j].pregunta.id){
-        					if(grupo[i].preguntasWrapperForUI[j].pregunta.tipoPregunta.tipoPregunta=='RadioButton'){
+        	for(var i = 0;i < grupo.length; i++){
+        		for(var j = 0; j < grupo[i].preguntasWrapperForUI.length; j++){
+        			for(var h = 0; h < cuestionario.respuestasPregunta.length; h++){
+        				if(cuestionario.respuestasPregunta[h].pregunta.id == grupo[i].preguntasWrapperForUI[j].pregunta.id){
+        					if(grupo[i].preguntasWrapperForUI[j].pregunta.tipoPregunta.tipoPregunta == 'RadioButton'){
         						cuestionario.respuestasPregunta[h].respuestaSeleccionada =grupo[i].preguntasWrapperForUI[j].respuesta.respuestaSeleccionada;
             				}
-        					if(grupo[i].preguntasWrapperForUI[j].pregunta.tipoPregunta.tipoPregunta=='TextArea'){
+        					if(grupo[i].preguntasWrapperForUI[j].pregunta.tipoPregunta.tipoPregunta == 'TextArea'){
         						cuestionario.respuestasPregunta[h].respuestaSeleccionada=grupo[i].preguntasWrapperForUI[j].respuesta.respuestaSeleccionada;
             				}
-        					if(grupo[i].preguntasWrapperForUI[j].pregunta.tipoPregunta.tipoPregunta=='Escala'){
+        					if(grupo[i].preguntasWrapperForUI[j].pregunta.tipoPregunta.tipoPregunta == 'Escala'){
         						cuestionario.respuestasPregunta[h].respuestaSeleccionada=grupo[i].preguntasWrapperForUI[j].respuesta.respuestaSeleccionada;
             				}
         				}
-        				k++;
         			}
         				
         		}
@@ -63,7 +61,7 @@
         	
         }
         
-        function onSaveSuccess (result) {
+        function onSaveSuccess(result) {
             $scope.$emit('campoApp:usuarioUpdate', result);
             $state.go('docente',{}, {reload: true});
         }
@@ -74,33 +72,32 @@
         }
         
         function anterior(indice){
-        	vm.validarseccion(indice);
-        	if(vm.valido)
-        	vm.show=indice;
+       		vm.show = indice;
+       		vm.mensaje = null;
+       		vm.valido = null;
         	
         }
         function siguiente(indice){
         	vm.validarseccion(indice);
         	if(vm.valido)
-        	vm.show=indice+2;
-        	
+        		vm.show = indice+2;
         }
         
         function validarseccion(seccion){
-        	var grupo=vm.resuelto.grupoPreguntasForUI;
-        	var cuestionario=vm.resuelto.cuestionarioResuelto;
-        	var valido=true;
-        	var mensaje=null;
-        	var k=0;
-        	vm.preguntainvalida=[];
-        	var index=0;
-        		for(var j=0;j<grupo[seccion].preguntasWrapperForUI.length;j++){
-        			for(var h=0;h<cuestionario.respuestasPregunta.length;h++){
+        	var grupo = vm.resuelto.grupoPreguntasForUI;
+        	var cuestionario = vm.resuelto.cuestionarioResuelto;
+        	var valido = true;
+        	var mensaje = null;
+        	var k = 0;
+        	vm.preguntainvalida = [];
+        	var index = 0;
+        		for(var j = 0; j < grupo[seccion].preguntasWrapperForUI.length; j++){
+        			for(var h = 0; h < cuestionario.respuestasPregunta.length; h++){
         				if(grupo[seccion].preguntasWrapperForUI[j].pregunta.obligatoria && cuestionario.respuestasPregunta[h].pregunta.id==grupo[seccion].preguntasWrapperForUI[j].pregunta.id){
 
         					if(grupo[seccion].preguntasWrapperForUI[j].respuesta.respuestaSeleccionada==null){
-        						valido=false;
-        						vm.preguntainvalida[index]=grupo[seccion].preguntasWrapperForUI[j].pregunta.id;
+        						valido = false;
+        						vm.preguntainvalida[index] = grupo[seccion].preguntasWrapperForUI[j].pregunta.id;
         						index++;
         					}
         					
@@ -110,18 +107,17 @@
         				
         		}
         		if(!valido){
-        			mensaje="No ha contestado todas las preguntas de esta sección";
+        			mensaje = "No has contestado todas las preguntas de esta sección.";
 				}
-        		vm.valido=valido;
-        		vm.mensaje=mensaje;
+        		vm.valido = valido;
+        		vm.mensaje = mensaje;
         }
         
         function contain(idpregunta){
-        	var invalido=false;
-        	//alert(vm.preguntainvalida.length);
-        	for(var index=0;index<vm.preguntainvalida.length;index++){
-        		if(vm.preguntainvalida[index]==idpregunta){
-        			invalido=true;
+        	var invalido = false;
+        	for(var index = 0;index < vm.preguntainvalida.length; index++){
+        		if(vm.preguntainvalida[index] == idpregunta){
+        			invalido = true;
         			break;
         		}
         			
