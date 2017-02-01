73da694264934fea5058562a948963455768e2c0
rodrigo.denis <rodrigo.denis@SG_SICEI_142144.inet.uady.mx>
2017-02-01 10:04:21 -0600

    #se agrego un servicio para impedir el cambio de fechas en la interfaz

--- src/main/webapp/app/actividadesevadoce/actividadesevadoce.service.js
+++ src/main/webapp/app/actividadesevadoce/actividadesevadoce.service.js
@@ -4,9 +4,9 @@
         .module('campoApp')
         .factory('ActividadesEvaluacion', ActividadesEvaluacion);
 
-    ActividadesEvaluacion.$inject = ['$resource'];
+    ActividadesEvaluacion.$inject = ['$resource','DateUtils'];
 
-    function ActividadesEvaluacion ($resource) {
+    function ActividadesEvaluacion ($resource,DateUtils) {
         var resourceUrl =  'apo/actividades/:id';
 
         return $resource(resourceUrl, {}, {
@@ -16,6 +16,7 @@
                 transformResponse: function (data) {
                     if (data) {
                         data = angular.fromJson(data);
+                        data.fin=DateUtils.convertLocalDateFromServer(data.fin);
                     }
                     return data;
                 }
